4f00d78a152ae7dc242a44cb22d4554ae1400894
Mehedi Ahsan <mdmehedicse01@gmail.com>
2023-08-23 23:53:16 +0600

    chunck array

---
+++ intermediate/chunkArray.js
@@ -0,0 +1,11 @@
+// Write a function that takes an array and a number (size) as inputs and returns an array of smaller arrays, each with a maximum length of size.
+
+function chunkArray(arr, size){
+    let chunk = [];
+    for (let i = 0; i < arr.length; i=i+size) {
+        chunk.push(arr.slice(i, i + size))
+    }
+    return chunk;
+}
+
+console.log(chunkArray([1,2,3,4,4,5,7,7,7,8,7],2))
