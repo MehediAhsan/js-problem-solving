2cf5e2771933444bbba86a63103280048914c136
Mehedi Ahsan <mdmehedicse01@gmail.com>
2023-10-16 18:13:00 +0600

    calc

---
+++ basic/calc.js
@@ -0,0 +1,13 @@
+const sum = (a,b) => {
+    return a+b;
+}
+const sub = (a,b) => {
+    return a-b;
+}
+const multi = (a,b) => {
+    return a*b;
+}
+const div = (a,b) => {
+    return a/b;
+}
+console.log(multi(2,3));
